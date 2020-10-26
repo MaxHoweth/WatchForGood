33bc1759f7b2d5672dc666f958ae60b863e87aa2
MaxHoweth <maxwellhoweth@gmail.com>
2020-10-25 20:44:00 -0700

    Finishing touches part one

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 /* ===================================================================
  * Dazzle - Main JS
  *
- * ------------------------------------------------------------------- */ 
+ * ------------------------------------------------------------------- */
 
 (function($) {
 
 	"use strict";
 
-	var cfg = {		
+	var cfg = {
 		scrollDuration : 800, // smoothscroll duration
 		mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'  // mailchimp url
-	},	
+	},
 
-	$WIN = $(window);	
+	$WIN = $(window);
 
    // Add the User Agent to the <html>
    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
 	var doc = document.documentElement;
 	doc.setAttribute('data-useragent', navigator.userAgent);
 
-	
-	/* Preloader 
+
+	/* Preloader
 	 * -------------------------------------------------- */
 	var ssPreloader = function() {
 
-		$WIN.on('load', function() {	
+		$WIN.on('load', function() {
 
 			// force page scroll position to top at page refresh
 			$('html, body').animate({ scrollTop: 0 }, 'normal');
 
          // will fade out the whole preloader DIV that covers the website.
 	      $("#preloader").delay(500).fadeOut('slow');
-	  
+
 	  	});
 	};
 
 
 	/* Mobile Menu
-	 * ---------------------------------------------------- */ 
+	 * ---------------------------------------------------- */
 	var ssMobileMenu = function() {
 
   		var toggleButton = $('.header-menu-toggle'),
@@ -57,22 +57,84 @@
 			else nav.removeClass('mobile');
 		});
 
-		$('#header-nav-wrap').find('a').on("click", function() {  
+		$('#header-nav-wrap').find('a').on("click", function() {
 
-			if (nav.hasClass('mobile')) {   		
-				toggleButton.toggleClass('is-clicked'); 
-				nav.slideToggle();   		
-			}     
+			if (nav.hasClass('mobile')) {
+				toggleButton.toggleClass('is-clicked');
+				nav.slideToggle();
+			}
 		});
 
-	}; 
+	};
+
+	/* CUSTOM COMPANY-INFO
+	* ------------------------------------------------------ */
+
+	document.getElementById("ourMissionButton").addEventListener("click", function(){
+			document.getElementById("companyInfoContent").innerHTML = 'Our mission is to use the power of social media to leverage support for nonprofits by bringing innovative ideas to the table through the use of disruptive technology, partnerships with consumer brands while fostering socio-economic responsibility within our communities.';
+			document.getElementById("ourMissionButton").style.background  ="#EC368D";
+			document.getElementById("founderBioButton").style.background  ="Transparent";
+			document.getElementById("aboutUsButton").style.background     ="Transparent";
+	});
+
+	document.getElementById("founderBioButton").addEventListener("click", function(){
+			document.getElementById("companyInfoContent").innerHTML = 'Kevin has worked in a variety of disciplines throughout his life, but never found his drive until he focused on giving back. Kevin attributes his passion for charity to his faith and believes whatever your hands find to do, do it with your might! Kevin also understands that behind every good cause there is a melting pot of courageous individuals, dreamers, willing to devote their time and energy to a possibility and understanding. Big ideas tend to challenge the norms of our society and are usually far bigger than a single human effort, it requires citizen participation and the collective ability to color outside the lines of traditional thinking.';
+			document.getElementById("ourMissionButton").style.background  ="Transparent";
+			document.getElementById("founderBioButton").style.background  ="#EC368D";
+			document.getElementById("aboutUsButton").style.background     ="Transparent";
+	});
+
+	document.getElementById("aboutUsButton").addEventListener("click", function(){
+			document.getElementById("companyInfoContent").innerHTML = 'Watch for Good, is a charitable social network and marketplace. Our goal is to increase the corporate social responsibility footprint within the giving community through partnerships, fundraising, and advocacy. We offer a cause based platform that allows brands and nonprofits organizations to share their political, social, and environmental values with our audience through video. What we do is monetize the content and share a portion of the advertising revenue with the nonprofits every time a video is viewed. Our modern approach to fundraising uses digital advertising in the crowdfunding space by converting viewer impressions into donations. Our cashless method of fundraising allows our users to focus on the cause, leaving them feeling invested while supporting the brands in our marketplace. ';
+			document.getElementById("ourMissionButton").style.background ="Transparent";
+			document.getElementById("founderBioButton").style.background  ="Transparent";
+			document.getElementById("aboutUsButton").style.background     ="#EC368D";
+	});
+
+	/* CUSTOM HOME VIDEO BACKGROUND AUTOPLAY
+	* ------------------------------------------------------ */
+	var video = document.getElementById("videobcg");
+	video.muted = true;
+	video.play();
+
+
+	/* CUSTOM FAQ
+	* ------------------------------------------------------ */
+	//FAQ Button Switchboard Array
+	var buttonStates = [false,false,false,false,false,false,false,false,false,false,false,false];
+
+	//Link all .faqMainButton class members to the function listener
+	document.querySelectorAll('.faqMainButton').forEach(item => {
+		item.addEventListener('click', handleFaqButtonClick);
+	})
+
+	function handleFaqButtonClick(event) {
+		const num = event.currentTarget.id.replace('faqButton','');
+
+		if(buttonStates[num] == false) {
+			document.getElementById("faqText" + num).style.display = "block";
+			document.getElementById("faqSpan" + num).innerHTML = "-";
+			buttonStates[num] = !buttonStates[num];
+		}
+		else {
+			document.getElementById("faqText" + num).style.display ="none";
+			document.getElementById("faqSpan" + num).innerHTML = "+";
+			buttonStates[num] = !buttonStates[num];
+		}
+
+
+	}
+
+
+
+
 
 
 	/* FitVids
 	 * ---------------------------------------------------- */
 	var ssFitVids = function() {
 		$(".fluid-video-wrapper").fitVids();
-	}; 
+	};
 
 
 
@@ -80,14 +142,21 @@
 	* ------------------------------------------------------ */
 	var ssOwlCarousel = function() {
 
-		$(".owl-carousel").owlCarousel({	
+		$(".owl-carousel").owlCarousel({
 	      loop: true,
   			nav: false,
-			autoHeight: true,
-  			items: 1
+				autoHeight: true,
+  			items: 1,
+
+
+				autoplay:true,
+    		autoplayTimeout:7500,
+				autoplaySpeed:1500,
 		});
 
-	};  	
+
+	};
+
 
 
   /* Highlight the current section in the navigation bar
@@ -95,7 +164,7 @@
 	var ssWaypoints = function() {
 
 		var sections = $("section"),
-		navigation_links = $(".header-main-nav li a");	
+		navigation_links = $(".header-main-nav li a");
 
 		sections.waypoint( {
 
@@ -107,12 +176,12 @@
 
 				if (direction === "up") active_section = active_section.prev();
 
-				var active_link = $('.header-main-nav li a[href="#' + active_section.attr("id") + '"]');			
+				var active_link = $('.header-main-nav li a[href="#' + active_section.attr("id") + '"]');
 
 	         navigation_links.parent().removeClass("current");
 				active_link.parent().addClass("current");
 
-			}, 
+			},
 
 			offset: '25%'
 
@@ -127,9 +196,9 @@
 		$('.smoothscroll').on('click', function (e) {
 			var target = this.hash,
 			$target    = $(target);
-	 	
+
 		 	e.preventDefault();
-		 	e.stopPropagation();	  
+		 	e.stopPropagation();
 
 			$('html, body').stop().animate({
 				'scrollTop': $target.offset().top
@@ -146,7 +215,7 @@
   /* Placeholder Plugin Settings
 	* ------------------------------------------------------ */
 	var ssPlaceholder = function() {
-		$('input, textarea, select').placeholder();  
+		$('input, textarea, select').placeholder();
 	};
 
 
@@ -157,10 +226,10 @@
 
   		$('.alert-box').on('click', '.close', function() {
 		  $(this).parent().fadeOut(500);
-		}); 
+		});
 
-  	};	  	
-	
+  	};
+
 
 
   /* Animate On Scroll
@@ -207,12 +276,12 @@
 		  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
 		  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
 		  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-		} 
+		}
 
 	};
 
 
- 
+
   /* Back to Top
 	* ------------------------------------------------------ */
 	var ssBackToTop = function() {
@@ -231,9 +300,9 @@
 				goTopButton.fadeOut(fadeOutTime);
 			}
 		});
-	};	
+	};
 
-  
+
    /* Initialize
 	* ------------------------------------------------------ */
 	(function ssInit() {
@@ -249,11 +318,11 @@
 		ssAOS();
 		ssBackToTop();
 
-		// to use the mailchimp form, uncomment the 
+		// to use the mailchimp form, uncomment the
 		// function call ssAjaxChimp() below:
-		// ssAjaxChimp(); 
+		// ssAjaxChimp();
 
 	})();
- 
+
 
 })(jQuery);
